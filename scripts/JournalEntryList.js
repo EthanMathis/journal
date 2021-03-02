e8b4ccfa894dcdc489c98956bdd676187a9d44f8
EthanMathis <ethan.mathis@outlook.com>
2021-03-02 01:12:36 -0600

    Javascript work in progress

---
+++ scripts/JournalEntryList.js
@@ -0,0 +1,19 @@
+// Purpose: To render a list of entry components 
+
+import { getJournalEntries } from "./JournalData.js"
+import { JournalEntryComponent } from "./JournalEntry.js"
+
+// const entryLog = document.querySelector("#entryLog")
+
+// export const EntryListComponent = () => {
+//     // Use the journal entry data from the data module component
+//     const entries = getJournalEntries()
+
+//     for (const entry of entries) {
+//         /*
+//             Invoke the component that returns an
+//             HTML representation of a single entry
+//         */
+//         entryLog.innerHTML += ???;
+//     }
+// }
