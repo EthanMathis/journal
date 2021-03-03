a128a52f7f28384090c9c351d7d7d04caa4159fb
EthanMathis <ethan.mathis@outlook.com>
2021-03-03 13:05:01 -0600

    added JS to insert journal entries into the entry-log on the DOM

--- scripts/JournalEntryList.js
+++ scripts/JournalEntryList.js
@@ -3,17 +3,15 @@
 import { getJournalEntries } from "./JournalData.js"
 import { JournalEntryComponent } from "./JournalEntry.js"
 
-// const entryLog = document.querySelector("#entryLog")
 
-// export const EntryListComponent = () => {
-//     // Use the journal entry data from the data module component
-//     const entries = getJournalEntries()
+export const EntryListComponent = () => {
+    // Use the journal entry data from the data module component
+    let entryHTML = "<h3>List of Entries</h3>";
+    const entryLog = document.querySelector(".entry-log")
+    const entries = getJournalEntries();
+    for (const entry of entries) {
+       entryHTML += JournalEntryComponent(entry);
 
-//     for (const entry of entries) {
-//         /*
-//             Invoke the component that returns an
-//             HTML representation of a single entry
-//         */
-//         entryLog.innerHTML += ???;
-//     }
-// }
+    };
+    entryLog.innerHTML += entryHTML;
+};
